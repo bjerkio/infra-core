15174e81bb6971f4ac51f31394d2be5bfeb7d358
Simen A. W. Olsen <so@bjerk.io>
2021-04-05 17:09:01 +0200

    fix: Update workflows and fix reference issue

--- src/internal/bjerk-io.ts
+++ src/internal/bjerk-io.ts
@@ -4,7 +4,7 @@ import { folder } from './folder';
 export const setup = new ProjectOnGithub('bjerk-io', {
   projectName: 'bjerk-io',
   folderId: folder.id,
-  repository: 'bjerkio/bjerk-io-inra',
+  repository: 'bjerk-io-infra',
   projectAliases: [
     'urn:pulumi:prod::bjerk-io-core::gcp:organizations/project:Project::bjerk-io',
   ],
