60f43787eca4187a2f8042e81f8fcdc343e0ab97
Simen A. W. Olsen <so@bjerk.io>
2021-04-05 16:43:14 +0200

    wip

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import * as pulumi from '@pulumi/pulumi';
 export const config = new pulumi.Config();
 export const billingAccount = config.requireSecret('billingAccount');
 export const organizationNumber = 904377566042;
-export const pulumiAccessToken = pulumi.secret(process.env.PULUMI_ACCESS_TOKEN);
-export const githubToken = pulumi.secret(process.env.GITHUB_TOKEN);
+export const pulumiAccessToken = pulumi.secret<string>(process.env.PULUMI_ACCESS_TOKEN);
+export const githubToken = pulumi.secret<string>(process.env.GITHUB_TOKEN);
