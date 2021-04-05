ed495370af4e54bca4d33d7f21e278d2191bcd68
Simen A. W. Olsen <cobraz@cobraz.no>
2021-04-05 16:50:19 +0200

    feat: add secrets to bjerk-io (#1)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 import * as pulumi from '@pulumi/pulumi';
 
 export const config = new pulumi.Config();
-export const billingAccount = config.getSecret('billingAccount');
+export const billingAccount = config.requireSecret('billingAccount');
 export const organizationNumber = 904377566042;
+export const pulumiAccessToken = pulumi.secret<string>(process.env.PULUMI_ACCESS_TOKEN);
+export const githubToken = pulumi.secret<string>(process.env.GITHUB_TOKEN);
