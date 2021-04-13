a95614c7e89c8cb0ed9a9949039c5a2608ba61cc
Simen A. W. Olsen <cobraz@cobraz.no>
2021-04-13 19:12:55 +0200

    Add Github Providers (#3)

--- src/internal/bjerk-io.ts
+++ src/internal/bjerk-io.ts
@@ -1,16 +1,22 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as gcp from '@pulumi/gcp';
+import * as github from '@pulumi/github';
 import { ProjectOnGithub } from '../components/projects-on-github';
 import { folder } from './folder';
+import { bjerkio } from '../github-orgs';
 
-export const setup = new ProjectOnGithub('bjerk-io', {
-  projectName: 'bjerk-io',
-  folderId: folder.id,
-  repository: 'bjerk-io-infra',
-  projectAliases: [
-    'urn:pulumi:prod::bjerk-io-core::gcp:organizations/project:Project::bjerk-io',
-  ],
-});
+export const setup = new ProjectOnGithub(
+  'bjerk-io',
+  {
+    projectName: 'bjerk-io',
+    folderId: folder.id,
+    repository: 'bjerk-io-infra',
+    projectAliases: [
+      'urn:pulumi:prod::bjerk-io-core::gcp:organizations/project:Project::bjerk-io',
+    ],
+  },
+  { providers: [bjerkio] },
+);
 
 export const dnsRole = new gcp.projects.IAMMember(
   'bjerk-io-dns-iam',
