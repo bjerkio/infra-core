6c404117c2ab92cac7417a811fec5fff3d464e17
Simen A. W. Olsen <cobraz@cobraz.no>
2021-04-13 20:13:09 +0200

    feat: Add bassene (#5)
    * feat: Add bassene
    
    * wip

---
+++ src/customers/bassene-web.ts
@@ -0,0 +1,24 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as gcp from '@pulumi/gcp';
+import { ProjectOnGithub } from '../components/projects-on-github';
+import { folder } from './folder';
+import { basssene } from '../github-orgs';
+
+export const setup = new ProjectOnGithub(
+  'bassene-web',
+  {
+    projectName: 'bassene-web',
+    folderId: folder.id,
+    repository: 'infra',
+  },
+  { providers: [basssene] },
+);
+
+export const dnsRole = new gcp.projects.IAMMember(
+  'bassene-web-dns-iam',
+  {
+    member: pulumi.interpolate`serviceAccount:${setup.serviceAccount.email}`,
+    role: 'roles/dns.admin',
+  },
+  { provider: setup.googleProvider },
+);
