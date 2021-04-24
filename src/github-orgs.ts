2e1ef581b1f7ab7b397e3122643f6178e6f02985
Simen A. W. Olsen <so@bjerk.io>
2021-04-24 23:00:38 +0200

    Set owner instead of org

--- src/github-orgs.ts
+++ src/github-orgs.ts
@@ -2,9 +2,8 @@ import * as github from '@pulumi/github';
 
 export const bjerkio = new github.Provider(`bjerkio-provider`, {
   owner: 'bjerkio',
-  organization: 'bjerkio',
 });
+
 export const basssene = new github.Provider(`basssene-provider`, {
   owner: 'basssene',
-  organization: 'basssene',
 });
