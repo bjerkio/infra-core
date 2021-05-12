9e270e221af3372d2eff800c51b0a12a0f8fb315
Simen A. W. Olsen <so@bjerk.io>
2021-05-12 23:52:20 +0200

    Add btools configuration (#11)

--- src/internal/btools.ts
+++ src/internal/btools.ts
@@ -1,6 +1,13 @@
 import * as gcp from '@pulumi/gcp';
+import * as pulumi from '@pulumi/pulumi';
+import * as github from '@pulumi/github';
 import { billingAccount } from '../config';
+import { ProjectOnGithub } from '../components/projects-on-github';
 import { folder } from './folder';
+
+export const githubProvider = new github.Provider(`btools-provider`, {
+  owner: 'btoolsorg',
+});
 
 export const project = new gcp.organizations.Project(
   'btools',
@@ -12,4 +19,22 @@ export const project = new gcp.organizations.Project(
     folderId: folder.id,
   },
   { protect: true },
+);
+
+export const setup = new ProjectOnGithub(
+  'btools-project',
+  {
+    project,
+    repository: 'infra',
+  },
+  { providers: [githubProvider] },
+);
+
+export const ownerRole = new gcp.projects.IAMMember(
+  'btools-owner-iam',
+  {
+    member: pulumi.interpolate`serviceAccount:${setup.serviceAccount.email}`,
+    role: 'roles/owner',
+  },
+  { provider: setup.googleProvider },
 );
